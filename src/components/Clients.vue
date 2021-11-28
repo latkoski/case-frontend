f632210a269e9f4127ad5c268584c979e8aa16c8
Angel Latkoski <a.latkoski@gmail.com>
2021-11-28 17:00:46 +0100

    deploy successful

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -11,63 +11,63 @@
       style="padding: 0 15%; display: flex; align-items: center"
     >
       <div class="col-xs-6 col-sm-3 client-logo">
-        <img class="client-logo-img" src="/logos/nivea.jpg" alt="Nivea" />
+        <img class="client-logo-img" src="logos/nivea.jpg" alt="Nivea" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo">
-        <img class="client-logo-img" src="/logos/mona.jpg" alt="Mona" />
+        <img class="client-logo-img" src="logos/mona.jpg" alt="Mona" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo">
         <img
           class="client-logo-img"
-          src="/logos/transavia.jpg"
+          src="logos/transavia.jpg"
           alt="transavia"
         />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo">
-        <img class="client-logo-img" src="/logos/zalando.jpg" alt="zalando" />
+        <img class="client-logo-img" src="logos/zalando.jpg" alt="zalando" />
       </div>
 
       <div class="col-xs-6 col-sm-3 client-logo">
-        <img class="client-logo-img" src="/logos/tomtom.jpg" alt="tomtom" />
+        <img class="client-logo-img" src="logos/tomtom.jpg" alt="tomtom" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo">
-        <img class="client-logo-img" src="/logos/unilever.jpg" alt="unilever" />
+        <img class="client-logo-img" src="logos/unilever.jpg" alt="unilever" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/adidas.jpg" alt="adidas" />
+        <img class="client-logo-img" src="logos/adidas.jpg" alt="adidas" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/pathe.jpg" alt="pathe" />
+        <img class="client-logo-img" src="logos/pathe.jpg" alt="pathe" />
       </div>
 
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/abn.jpg" alt="abn" />
+        <img class="client-logo-img" src="logos/abn.jpg" alt="abn" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/triumph.jpg" alt="triumph" />
+        <img class="client-logo-img" src="logos/triumph.jpg" alt="triumph" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
         <img
           class="client-logo-img"
-          src="/logos/microsoft.jpg"
+          src="logos/microsoft.jpg"
           alt="microsoft"
         />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/oxxio.jpg" alt="oxxio" />
+        <img class="client-logo-img" src="logos/oxxio.jpg" alt="oxxio" />
       </div>
 
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/nn.jpg" alt="Ninnvea" />
+        <img class="client-logo-img" src="logos/nn.jpg" alt="Ninnvea" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/ziggo.jpg" alt="ziggo" />
+        <img class="client-logo-img" src="logos/ziggo.jpg" alt="ziggo" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/walibi.jpg" alt="walibi" />
+        <img class="client-logo-img" src="logos/walibi.jpg" alt="walibi" />
       </div>
       <div class="col-xs-6 col-sm-3 client-logo desktop-only">
-        <img class="client-logo-img" src="/logos/klm.jpg" alt="klm" />
+        <img class="client-logo-img" src="logos/klm.jpg" alt="klm" />
       </div>
     </div>
   </div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
 export const POSTS_URL =
-  "https://mocki.io/v1/028225d3-bb44-4da9-adad-c5fed17b5df1";
+  "https://mocki.io/v1/de7e76a0-71b1-43fe-b04e-db5342ceb7de";
